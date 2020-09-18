e8fbfb038b0b5fbcfdd13c943001e14b3b00fea1
jvanachteren <j.vanachteren@tweb.nl>
2020-09-18 13:59:03 +0200

    Level 3

--- NUnitTestProject/Model_Competition_NextTrackShould.cs
+++ NUnitTestProject/Model_Competition_NextTrackShould.cs
@@ -25,5 +25,68 @@ namespace ControllerTest
             Track result = _competition.NextTrack();
             Assert.IsNull(result);
         }
+
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            SectionTypes[] TestTrack = {
+                SectionTypes.StartGrid,
+                SectionTypes.RightCorner,
+                SectionTypes.LeftCorner,
+                SectionTypes.Finish
+            };
+            Track track = new Track("Test track", TestTrack);
+            _competition.Tracks.Enqueue(track);
+
+            Track result = _competition.NextTrack();
+            Assert.AreEqual(track, result);
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            SectionTypes[] TestTrack = {
+                SectionTypes.StartGrid,
+                SectionTypes.RightCorner,
+                SectionTypes.LeftCorner,
+                SectionTypes.Finish
+            };
+            Track track = new Track("Test track", TestTrack);
+            _competition.Tracks.Enqueue(track);
+            Track result = _competition.NextTrack();
+            result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            SectionTypes[] TestTrack1 = {
+                SectionTypes.StartGrid,
+                SectionTypes.RightCorner,
+                SectionTypes.LeftCorner,
+                SectionTypes.Finish
+            };
+
+            SectionTypes[] TestTrack2 = {
+                SectionTypes.StartGrid,
+                SectionTypes.RightCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.LeftCorner,
+                SectionTypes.Finish
+            };
+
+            Track track1 = new Track("Test track 1", TestTrack1);
+            _competition.Tracks.Enqueue(track1);
+
+            Track track2 = new Track("Test track 2", TestTrack2);
+            _competition.Tracks.Enqueue(track2);
+
+            Track result1 = _competition.NextTrack();
+            Track result2 = _competition.NextTrack();
+
+            Assert.AreEqual(result1, track1);
+            Assert.AreEqual(result2, track2);
+        }
     }
 }
